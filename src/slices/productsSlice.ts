67c31edab0bee0e2e6bc6a85ab93cc24b040c81e
Top Coder <topcoder.premiumware@gmail.com>
2023-03-19 20:41:00 +0200

    fixed prettier settings

--- src/slices/productsSlice.ts
+++ src/slices/productsSlice.ts
@@ -3,14 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {};
 
 export const productsSlice = createSlice({
-    name: "products",
-    initialState,
-    reducers: {
-        getProducts: () => {},
-        setProducts: (state: any, action) => {
-            state.products = action.payload;
-        },
+  name: "products",
+  initialState,
+  reducers: {
+    getProducts: () => {},
+    setProducts: (state: any, action) => {
+      state.products = action.payload;
     },
+  },
 });
 
 export const { getProducts, setProducts } = productsSlice.actions;
