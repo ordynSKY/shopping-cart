ede0d5d7f532d19c1d088506803fa077fe28d457
Top Coder <topcoder.premiumware@gmail.com>
2023-03-19 20:51:42 +0200

    fixed config eslint

--- src/saga/request.ts
+++ src/saga/request.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const fetchProducts = async () => {
-  console.log("req; ", process.env);
   try {
     const data = await axios.get(
       `${process.env.REACT_APP_BASE_URL}products` || ""
